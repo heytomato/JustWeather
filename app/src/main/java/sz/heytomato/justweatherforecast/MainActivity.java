f3b9b551d5f251aa7029911f14c15f74e4ede039
Heytomato <lxxfs@foxmail.com>
2016-05-23 21:14:47 +0800

    v1.1版本

--- app/src/main/java/sz/heytomato/justweatherforecast/MainActivity.java
+++ app/src/main/java/sz/heytomato/justweatherforecast/MainActivity.java
@@ -3,13 +3,14 @@ package sz.heytomato.justweatherforecast;
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,6 +28,11 @@ import com.thinkpage.lib.api.TPWeatherDaily;
 import com.thinkpage.lib.api.TPWeatherManager;
 import com.thinkpage.lib.api.TPWeatherNow;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -35,70 +41,35 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
 
-    protected ViewPager viewPager;
-    protected ViewPagerAdapter viewPagerAdapter;
-    protected List<View> views;
-    protected ImageView[] points;
-    protected int[] ids={R.id.point_t,R.id.point_s};
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private List<View> views;
+    private ImageView[] points;
+    private int[] ids={R.id.point_t,R.id.point_s};
 
 
-    protected LinearLayout weatherInfoLayout;
-    /**
-     * 用于显示城市名
-     * */
-    protected TextView cityNameText;
-    /**
-     * 用于显示发布时间
-     */
-    protected TextView publishText;
-    /**
-     * 用于显示天气描述信息
-     */
-    protected TextView weatherDespText;
-
-
-    /**
-     * 切换城市按钮
-     */
-    /**
-     * 更新天气按钮
-     */
-    protected ImageView refreshWeather;
-
-    protected ImageView weatherPicture;
-
-    protected TextView temp;
-
-    protected ListView weatherDaily;
-
-//    protected EditText editText;
-
-    protected String getEditText;
-
-    protected TextView weatherFuture;
-
-
-
-
-
-
-//    public WeatherDaily day1=new WeatherDaily("Day1","- -","- -",R.drawable.w99);
-//    public WeatherDaily day2=new WeatherDaily("Day2","- -","- -",R.drawable.w99);
-//    public WeatherDaily day3=new WeatherDaily("Day3","- -","- -",R.drawable.w99);
-//    public WeatherDaily day4=new WeatherDaily("Day4","- -","- -",R.drawable.w99);
-//    public WeatherDaily day5=new WeatherDaily("Day5","- -","- -",R.drawable.w99);
-
-
+    private LinearLayout weatherInfoLayout;
+    private TextView cityNameText;
+    private TextView publishText;
+    private TextView weatherDespText;
+    private ImageView refreshWeather;
+    private ImageView weatherPicture;
+    private TextView temp;
+    private ListView weatherDaily;
+    private String getEditText;
+    private TextView weatherFuture;
+    private int mBackKeyPressedTimes;
+    private Boolean isFirstIn;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
+        init();
         initViews();
         initPoint();
+        readData();
 
     }
 
@@ -114,7 +85,7 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         switch (item.getItemId()){
             case R.id.change_city:
                 final EditText editText=new EditText(this);
-                editText.setHint("请输入城市名称");
+                editText.setHint("请输入城市名称 例：“深圳”");
                 final AlertDialog.Builder dialog=new AlertDialog.Builder(this);
                 dialog.setTitle("切换城市");
                 dialog.setCancelable(true);
@@ -125,16 +96,11 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
                     public void onClick(DialogInterface dialogInterface, int i) {
                         getEditText=editText.getText().toString();
                         showWeather();
-
                     }
                 });
 
                 dialog.setNegativeButton("取消", null);
-
                 dialog.show();
-
-
-
                 break;
             case R.id.info:
                 AlertDialog.Builder dialogi=new AlertDialog.Builder(MainActivity.this);
@@ -144,35 +110,25 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
                 dialogi.setNeutralButton("关闭",null);
                 dialogi.show();
                 break;
-
         }
         return true;
     }
 
-    protected void initViews(){
+    private void initViews(){
         LayoutInflater layoutInflater=LayoutInflater.from(this);
-
         views=new ArrayList<View>();
         views.add(layoutInflater.inflate(R.layout.today,null));
         views.add(layoutInflater.inflate(R.layout.seven_day,null));
 
-
         viewPagerAdapter=new ViewPagerAdapter(views,this);
         viewPager=(ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(viewPagerAdapter);
-
-
-
         weatherInfoLayout = (LinearLayout)views.get(0).findViewById(R.id.weather_info_layout);
         cityNameText = (TextView)views.get(0). findViewById(R.id.city_name);
         publishText = (TextView)views.get(0). findViewById(R.id.publish_text);
         weatherDespText = (TextView)views.get(0). findViewById(R.id.weather_desp);
         weatherPicture=(ImageView)views.get(0).findViewById(R.id.weather_picture);
-
         temp= (TextView) views.get(0).findViewById(R.id.temp);
-//        switchCity = (Button)views.get(0). findViewById(R.id.switch_city);
-//        refreshWeather = (Button)views.get(0). findViewById(R.id.refresh_weather);
-
 
         refreshWeather= (ImageView) findViewById(R.id.refresh_weather);
         refreshWeather.setOnClickListener(new View.OnClickListener() {
@@ -184,35 +140,42 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
                 }else {
                     showWeather();
                 }
-//                final ProgressDialog progressDialog=new ProgressDialog(MainActivity.this);
-//                progressDialog.setMessage("正在更新天气...");
-////                progressDialog.setCancelable(true);
-//
-//                progressDialog.show();
-//                new Thread(){
-//                    @Override
-//                    public void run() {
-//                        try {
-//                            Thread.sleep(2000);
-//                            progressDialog.dismiss();
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//
-//                    }
-//                }.start();
-                }
-
+            }
         });
 
         weatherFuture= (TextView) views.get(1).findViewById(R.id.weather_future);
         weatherDaily=(ListView) views.get(1).findViewById(R.id.weather_daily);
         weatherDaily.setAdapter(new WeatherDailyAdapter(this));
-
         viewPager.setOnPageChangeListener(this);
+    }
+
+    private void init(){
+        SharedPreferences preferences=getSharedPreferences("one",MODE_PRIVATE);
+        isFirstIn=preferences.getBoolean("isFirstIn",true);
+        if (isFirstIn){
+            final EditText editText=new EditText(this);
+            editText.setHint("请输入城市名称 例：“深圳”");
+            final AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+            dialog.setTitle("选择城市");
+            dialog.setCancelable(true);
+            dialog.setView(editText);
+
+            dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    getEditText=editText.getText().toString();
+                    showWeather();
+                }
+            });
+            dialog.setNegativeButton("取消", null);
+            dialog.show();
 
 
-
+        }else {
+        }
+        SharedPreferences.Editor editor=preferences.edit();
+        editor.putBoolean("isFirstIn",false);
+        editor.commit();
     }
 
 
@@ -221,10 +184,7 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
     public void showWeather(){
 
-
         final Resources resources=getResources();
-
-
         final TPWeatherManager weatherManager = TPWeatherManager.sharedWeatherManager();
         weatherManager.initWithKeyAndUserId("kjhcscay7iilh3qf","U5AF3E16E8");
 
@@ -238,8 +198,6 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
                         if (tpWeatherNow != null) {
                             final ProgressDialog progressDialog=new ProgressDialog(MainActivity.this);
                             progressDialog.setMessage("正在更新天气...");
-                            //                progressDialog.setCancelable(true);
-
                             progressDialog.show();
                             new Thread(){
                                 @Override
@@ -250,7 +208,6 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
                                     } catch (InterruptedException e) {
                                         e.printStackTrace();
                                     }
-
                                 }
                             }.start();
 
@@ -259,16 +216,13 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
                             cityNameText.setText(getEditText);
                             weatherDespText.setText(tpWeatherNow.text);
                             temp.setText(String.valueOf(tpWeatherNow.temperature)+"℃");
-                            publishText.setText(simpleDateFormat.format(tpWeatherNow.lastUpdateDate));
+                            publishText.setText(simpleDateFormat.format(tpWeatherNow.lastUpdateDate)+"更新");
 
                         }else {
-                            Log.d("x",getEditText);
                             Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-
-
 
         Calendar calendar=Calendar.getInstance();
         calendar.setTime(new Date(System.currentTimeMillis()));
@@ -285,7 +239,6 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
                         SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd日");
                         for (int i =0;i < tpWeatherDailies.length;i++){
-
                             View w=new WeatherDailyAdapter(MainActivity.this).getView(i,weatherDaily.getChildAt(i), null);
                             TextView day= (TextView)w.findViewById(R.id.daily_date);
                             TextView text= (TextView) w.findViewById(R.id.daily_text);
@@ -301,15 +254,12 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
                         }
                         weatherFuture.setText(getEditText+"未来5天天气");
                     }else{
-
                     }
-
                 }
             });
-
     }
 
-    protected void initPoint(){
+    private void initPoint(){
         points=new ImageView[views.size()];
         for (int i=0;i<views.size();i++){
             points[i]= (ImageView) findViewById(ids[i]);
@@ -334,9 +284,66 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
     @Override
     public void onPageScrollStateChanged(int state) {
-
     }
 
+    @Override
+    public void onBackPressed() {
+        if (mBackKeyPressedTimes == 0) {
+            Toast.makeText(this, "再按一次退出程序 ", Toast.LENGTH_SHORT).show();
+            mBackKeyPressedTimes = 1;
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        mBackKeyPressedTimes = 0;
+                    }
+                }
+            }.start();
+            return;
+        }
+            else{
+               finish();
+            }
+        super.onBackPressed();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveData();
+    }
 
+    private void saveData(){
+        try {
+            OutputStream outputStream=openFileOutput("data", Context.MODE_PRIVATE);
+            outputStream.write(getEditText.getBytes("utf-8"));
+            outputStream.flush();
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readData(){
+        try {
+            InputStream inputStream=openFileInput("data");
+            byte[] bytes=new byte[inputStream.available()];
+            inputStream.read(bytes);
+            inputStream.close();
+            getEditText=new String(bytes,"utf-8");
+            showWeather();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
